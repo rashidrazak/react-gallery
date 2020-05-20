ca18b1f359bb61b0c2b43845d62cee3b4e62f3bb
Rashid Razak <bib.nexus@gmail.com>
2020-05-21 03:05:26 +0800

    Convert element to a controlled element (previously uncontrolled)

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,37 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value)
-  }
+  state = { term: '' }
 
+  /**
+   * NOTE:
+   * 
+   * If you prefer to call a helper function within onChange,
+   * use arrow function to preserve 'this'
+   * 
+   * In JSX:
+   * <input 
+   *   type="text"
+   *   value={this.state.term}
+   *   onChange={(event) => this.onInputChange(event)}
+   * />
+   * 
+   * In helper function:
+   * onInputChange(event) {
+   *   this.setState({ term: event.target.value })
+   * }
+   */
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input 
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })} 
+            />
           </div>
         </form>
       </div>
