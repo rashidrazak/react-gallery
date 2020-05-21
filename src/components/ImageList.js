06b9e718e3c4c9e57de0d6c65a69d6eee64c40b2
Rashid Razak <bib.nexus@gmail.com>
2020-05-22 04:09:19 +0800

    Best practice - add key prop for rendering list

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const ImageList = (props) => {
   const images = props.images.map((image) => {
-    return <img src={image.urls.regular} alt={image.alt_description} />
+    return <img key={image.id} src={image.urls.regular} alt={image.alt_description} />
   })
+
   return (
     <div>{images}</div>
   )
