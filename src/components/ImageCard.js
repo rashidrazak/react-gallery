54e3cede3cba5fccb5eb6a000cba768c0d0a0009
Rashid Razak <bib.nexus@gmail.com>
2020-05-23 01:29:39 +0800

    Use React.createRef() to get image height

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,29 @@
 import React from 'react'
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.imageRef = React.createRef()
+  }
+
+  componentDidMount() {
+    console.log('imageRef:', this.imageRef)
+
+    // Must attach an event listener to detect when the image has been loaded
+    this.imageRef.current.addEventListener('load', this.setSpans.bind(this))
+  }
+
+  setSpans() {
+    console.log('clientHeight', this.imageRef.current.clientHeight)
+  }
+
   render() {
     const {urls, alt_description} = this.props.image
-    
+
     return (
       <div>
         <img
+          ref={this.imageRef}
           src={urls.regular}
           alt={alt_description}
         />
