37b090d0179c89b695ab36608e68308f72ed5e63
Rashid Razak <bib.nexus@gmail.com>
2020-05-23 00:44:57 +0800

    Create ImageCard component and use it inside ImageList component

---
+++ src/components/ImageCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class ImageCard extends React.Component {
+  render() {
+    const {urls, alt_description} = this.props.image
+    
+    return (
+      <div>
+        <img
+          src={urls.regular}
+          alt={alt_description}
+        />
+      </div>
+    )
+  }
+}
+
+export default ImageCard
