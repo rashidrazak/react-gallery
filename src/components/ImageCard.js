1e9b592f738a3e2ae49977f38fbbd33f07f763fb
Rashid Razak <bib.nexus@gmail.com>
2020-05-23 01:42:52 +0800

    Dynamically set gridRowEnd value

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,6 +3,7 @@ import React from 'react'
 class ImageCard extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { spans: 0 }
     this.imageRef = React.createRef()
   }
 
@@ -14,14 +15,17 @@ class ImageCard extends React.Component {
   }
 
   setSpans() {
-    console.log('clientHeight', this.imageRef.current.clientHeight)
+    const height = this.imageRef.current.clientHeight
+    const spans = Math.ceil(height / 10)
+
+    this.setState({ spans })
   }
 
   render() {
     const {urls, alt_description} = this.props.image
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}`}}>
         <img
           ref={this.imageRef}
           src={urls.regular}
